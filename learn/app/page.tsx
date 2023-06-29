d9f2cddd63eb2111bd939aedb77bd6abc26e8526
Igor Parfenov <lloopy123123543@gmail.com>
2023-06-29 22:51:02 +0500

    sus

---
+++ learn/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="w-full mx-auto h-screen ">
+      <div className="w-full mx-auto container">
+        sisi
+      </div>
+    </main>
+  )
+}
